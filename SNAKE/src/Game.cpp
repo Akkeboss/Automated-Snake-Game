9db02fbdf59528cd89ae59a877fdae78897584e7
Axel Fransson <franssonaxel04@gmail.com>
2026-02-04 10:05:54 +0100

    Cleanup

--- SNAKE/src/Game.cpp
+++ SNAKE/src/Game.cpp
@@ -7,13 +7,8 @@ void Game::CreateFood()
 {
     /* 
         Add so the food can not spawn inside the snake
-
-        Solution
-        Check if the row and col pos of the food is not the same as any
-        pos in the snake body and head.
-        If it is generate a new food piece
-        Otherwise spawn the food in a valid position.
     */
+
     int count = 0;
     this->m_Food = new Food();
     int Row = this->m_Food->GetFoodRowPos();
@@ -64,7 +59,6 @@ void Game::SpawnSnake()
 
 void Game::RemoveOldSnake()
 {
-    //Need to set the last previous pos to '.'
     if(m_Snake.CurrSize() == 1)
     {
         m_Map.SetMapPos(m_Snake.GetSnakeHeadRowPos(), m_Snake.GetSnakeHeadColPos(), '.');
@@ -84,17 +78,6 @@ int Game::CalcNextMove()
         2 is backward
         3 is downward
         4 is upward
-        
-        Problem right now, in iteration 157 it has to valid moves
-        but returns 0 for some reason which means it think it has no
-        valid moves to do.
-
-        The problem is that the snake gets stuck because it cant move forward
-        because the col pos is > then the col pos of the snake. Makes it not valid
-        And to move downward the row pos has to be < smaller then the food. Which is false to.
-
-        Solution, maybe make a spefific case for this problem. Like if it cant move anywhere just move the snake
-        to a valid position until it use the row/col pos can work
     */
 
     //Move forward columwise
@@ -112,7 +95,6 @@ int Game::CalcNextMove()
         m_Snake.SetSnakeHeadCharacter('>');
         return 2; //Backwards
     }
-    //Valid move here returns false which is wrong because it is a valid move?? 
     else if(m_Snake.ValidMove(m_Snake.NextPos(3), m_Snake.GetSnakeHeadColPos()) && m_Snake.GetSnakeHeadRowPos() < m_Food->GetFoodRowPos())
     {
         PrevMove = 3;
@@ -146,13 +128,9 @@ int Game::CalcNextMove()
     return 0;
 }
 
-//Check over this
+//Move the snake to next position
 void Game::MoveSnake()
 {
-    /* 
-        This get called after the removal of snake
-        Needs to work for 1, 2, 3 and 4
-    */
     m_Snake.MoveSnake(CalcNextMove()); //Get the next move and update the whole snake
 
     int Row = m_Snake.GetSnakeHeadRowPos();
@@ -163,9 +141,7 @@ void Game::MoveSnake()
     if(Row >= m_Map.GetRows())
     {
         {
-            //std::cout << "No higher row to move to: Set Row to 0!\n";
             m_Snake.SetSnakeHeadRowPos(0);
-            //std::cout << "Set Snake head position at: " << m_Snake.GetSnakeHeadRowPos() << std::endl;
             m_Map.SetMapPos(m_Snake.GetSnakeHeadRowPos(), Col, Head);
         }
         if(m_Snake.CurrSize() > 1)
@@ -174,17 +150,14 @@ void Game::MoveSnake()
             {
                 int RowBody = m_Snake.GetSpecificRowPos(i);
                 int ColBody = m_Snake.GetSpecificColPos(i);
-                //std::cout << "Set the snake body: " << RowBody << " " << ColBody << std::endl;
                 m_Map.SetMapPos(RowBody, ColBody, 'o');
             }
         }
     }
     else if(Row < 0)
     {
-        //std::cout << "No lower rows to move to: Set Row to Max Row Size: " << m_Map.GetRows() << std::endl;
         {
             m_Snake.SetSnakeHeadRowPos(m_Map.GetRows() - 1);
-            //std::cout << "Set Snake head position at: " << m_Snake.GetSnakeHeadRowPos() << " " << Col << std::endl;
             m_Map.SetMapPos(m_Snake.GetSnakeHeadRowPos(), Col, Head); //Set head correctly
         }
         if(m_Snake.CurrSize() > 1)
@@ -193,17 +166,14 @@ void Game::MoveSnake()
             {
                 int RowBody = m_Snake.GetSpecificRowPos(i);
                 int ColBody = m_Snake.GetSpecificColPos(i);
-                //std::cout << "Set the snake body: " << RowBody << " " << ColBody << std::endl;
                 m_Map.SetMapPos(RowBody, ColBody, 'o');
             }
         }
     }
     else if(Col < 0)
     {
-        //std::cout << "No lower colums to move to: Set Colum to Max Colum Size: " << m_Map.GetCols() << std::endl;
         {
             m_Snake.SetSnakeHeadColPos(m_Map.GetCols() - 1);
-            //std::cout << "Set Snake head position at: " << m_Snake.GetSnakeHeadRowPos() << " " << Col << std::endl;
             m_Map.SetMapPos(m_Snake.GetSnakeHeadRowPos(), Col, Head); //Set head correctly
         }
         //Set body
@@ -213,7 +183,6 @@ void Game::MoveSnake()
             {
                 int RowBody = m_Snake.GetSpecificRowPos(i);
                 int ColBody = m_Snake.GetSpecificColPos(i);
-                //std::cout << "Set the snake body: " << RowBody << " " << ColBody << std::endl;
                 m_Map.SetMapPos(RowBody, ColBody, 'o');
             }
         }
@@ -221,10 +190,8 @@ void Game::MoveSnake()
     }
     else if(Col >= m_Map.GetCols())
     {
-        //std::cout << "No higher colums to move to: Set Colum to 0: " << std::endl;
         {
             m_Snake.SetSnakeHeadColPos(0);
-            //std::cout << "Set Snake head position at: " << m_Snake.GetSnakeHeadColPos() << " " << Col << std::endl;
             m_Map.SetMapPos(Row, m_Snake.GetSnakeHeadColPos(), Head); //Set head correctly
         }
         //Set body
@@ -234,7 +201,6 @@ void Game::MoveSnake()
             {
                 int RowBody = m_Snake.GetSpecificRowPos(i);
                 int ColBody = m_Snake.GetSpecificColPos(i);
-                //std::cout << "Set the snake body: " << RowBody << " " << ColBody << std::endl;
                 m_Map.SetMapPos(RowBody, ColBody, 'o');
             }
         }
@@ -243,17 +209,13 @@ void Game::MoveSnake()
     else
     {
         m_Map.SetMapPos(Row, Col, Head); //Set head on map //Correct
-        //std::cout << "Set Snake head position at: " << Row << " " << Col << std::endl;
-
         if(m_Snake.CurrSize() > 1)
         {
-            //std::cout << "Current size: " << m_Snake.CurrSize() << "\n"; // 2
             for(int i = 1; i < m_Snake.CurrSize(); i++)
             {
                 //All current pos
                 int RowBody = m_Snake.GetSpecificRowPos(i);
                 int ColBody = m_Snake.GetSpecificColPos(i);
-                //std::cout << "Set snake body: " << RowBody << " " << ColBody << std::endl;
                 m_Map.SetMapPos(RowBody, ColBody, 'o');
             }
         }    
@@ -319,6 +281,7 @@ void Game::DisplayResult() const
     std::cout << "Steps taken by the snake is: " << StepsTaken << std::endl;
 }
 
+//Main Game Loop
 void Game::run()
 {
     /* 
@@ -330,7 +293,7 @@ void Game::run()
 
     */
     IsSnakeDead = false;
-    int count = 0;
+    int count = 0; //Debugging purpose
     CreateFood();
     SpawnSnake();
     RenderMap();
